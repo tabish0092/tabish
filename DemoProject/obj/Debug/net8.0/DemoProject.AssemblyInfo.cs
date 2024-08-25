acfce559dfc75220bef6b17fa71b18e0b46a815b
tabish00092 <mohdtabish090@outlook.com>
2024-08-25 21:01:46 +0530

    Docker file added

--- DemoProject/obj/Debug/net8.0/DemoProject.AssemblyInfo.cs
+++ DemoProject/obj/Debug/net8.0/DemoProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DemoProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54079e5a2cf05ae7b2d9ed45e11f4de2b8d3c274")]
 [assembly: System.Reflection.AssemblyProductAttribute("DemoProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DemoProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
